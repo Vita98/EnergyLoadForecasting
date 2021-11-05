ffe2a8dae3b7b5850f446a143b06f0364747eccf
Vitandrea Sorino <vitandrea98@live.it>
2021-11-05 16:07:42 +0100

    Some changes to the main script plus home 1 boiler forniture one week training and forecasting

--- arima.py
+++ arima.py
@@ -7,91 +7,146 @@ from sklearn.metrics import mean_squared_error
 from math import sqrt
 import pandas as pd
 import os
+import enum
 
-# 1440 = 1 day, 10080 = 7 days, 43200 = 30 days
-numbersOfRowToRead = 44640
-trainSize = 43200
-testSize = 1440
+class TrainignTimeType(enum.IntEnum):
+	ONE_WEEK = 10080
+	ONE_MONTH = 43200
 
-originFileName = "ukdale_def4.csv"
-seriesName = "Gas_Boiler"
-
+class TestingTimeType(enum.IntEnum):
+	ONE_DAY = 1440
 
 
+
+
+
+
+'''
+	PUT HERE THE CONFIGURATION VALUES
+										'''
+trainSize = TrainignTimeType.ONE_WEEK
+testSize = TestingTimeType.ONE_DAY
+
+originFileName = "ukdale_def1.csv"
+seriesName = "Boiler"
+
+
+
+
+
+
+
+
+#Save the time series given as parameter 
 def save_series_to_csv(series, fileName):
 	path = "result/" + originFileName[:-4]
-	try:
-		os.mkdir(path)
-	except OSError:
-		print("Creation of the directory %s failed" % path)
+
+	if not os.path.isdir(path):
+		try:
+			os.mkdir(path)
+		except OSError:
+			print("Creation of the directory %s failed" % path)
 
 	path = "result/" + originFileName[:-4] + "/" + seriesName
-	try:
-		os.mkdir(path)
-	except OSError:
-		print("Creation of the directory %s failed" % path)
+
+	if not os.path.isdir(path):
+		try:
+			os.mkdir(path)
+		except OSError:
+			print("Creation of the directory %s failed" % path)
 
 	day = trainSize / 1440
 	file = open(path + "/" + str(int(day)) + "days_" + fileName, "w")
-	file.write(series.to_csv())
+	file.write(series.to_csv(header=False))
 	file.close()
 
 
+#Save the plot from pyplot
+def save_plot():
+	path = "result/" + originFileName[:-4]
 
+	if not os.path.isdir(path):
+		try:
+			os.mkdir(path)
+		except OSError:
+			print("Creation of the directory %s failed" % path)
+
+	path = "result/" + originFileName[:-4] + "/" + seriesName
+	if not os.path.isdir(path):
+		try:
+			os.mkdir(path)
+		except OSError:
+			print("Creation of the directory %s failed" % path)
+
+	day = trainSize / 1440
+	finalPath = path + "/" + str(int(day)) + "days_plot.png"
+	pyplot.savefig(finalPath, dpi=100)
+
+#Parser for the read_csv
 def parser(x):
 	return datetime.strptime(x, '%y-%m-%d %H:%M:%S')
 
+#main function
+def main():
+	numbersOfRowToRead = int(trainSize) + int(testSize)
+
+    #Reading the series from the dataset file
+	series = read_csv("Dataset/" + originFileName,header=0,index_col=0,nrows=numbersOfRowToRead)
+	print(series[seriesName].head())
+
+	#Splitting the dataset into training and testing 
+	X = series[seriesName]
+	train, test = X[0:trainSize], X[trainSize:trainSize+testSize]
+	history = [x for x in train]
+	predictions = list()
+
+	#Creating the ARIMA model
+	print("\nTraining the model...\n")
+	model = ARIMA(history, order=(5,0,1))
+	model_fit = model.fit(start_params=[0,0,0,0,0,0,0,1])
+	maxLen = len(test)
 
 
-series = read_csv("Dataset/" + originFileName,header=0,index_col=0,nrows=numbersOfRowToRead)
-print(series[seriesName].head())
+	print("Testing...")
+	# walk-forward validation
+	for t in range(len(test)):
 
+		perc = (100 / maxLen) * t
+		print("\nPerc: %.2f%%" %perc,end="\r")
+		print ("\033[A                             \033[A")
 
-X = series[seriesName]
-train, test = X[0:trainSize], X[trainSize:trainSize+testSize]
-history = [x for x in train]
-predictions = list()
+		output = model_fit.forecast()
+		yhat = output[0]
+		predictions.append(yhat)
+		obs = test[t]
+		history.append(obs)
 
+		model_fit = model_fit.append([test[t]])
+		#print('predicted=%f, expected=%f' % (yhat, obs))
 
-model = ARIMA(history, order=(5,0,1))
-model_fit = model.fit(start_params=[0,0,0,0,0,0,0,1])
-maxLen = len(test)
+	#add time index to predictions
+	fc_series = pd.Series(predictions,index=test.index)
 
+	# evaluate forecasts
+	rmse = sqrt(mean_squared_error(test, predictions))
+	print('Test RMSE: %.3f' % rmse)
 
-# walk-forward validation
-for t in range(len(test)):
+	# plot forecasts against actual outcomes
+	pyplot.figure(figsize=(12,5), dpi=100)
+	pyplot.plot(train, color='blue')
+	pyplot.plot(test, color='blue')
+	pyplot.plot(fc_series, color='red')
+	ax = pyplot.gca()
+	ax.axes.xaxis.set_visible(False)
 
-	perc = (100 / maxLen) * t
-	print("\nPerc: %.2f" %perc)
+	#saving date
+	save_series_to_csv(train, "train.csv")
+	save_series_to_csv(test, "test.csv")
+	save_series_to_csv(fc_series, "predictions.csv")
+	save_plot()
+	#pyplot.show()
 
-	output = model_fit.forecast()
-	yhat = output[0]
-	predictions.append(yhat)
-	obs = test[t]
-	history.append(obs)
+	print("\nAll done!\n")
 
-	model_fit = model_fit.append([test[t]])
-	#print('predicted=%f, expected=%f' % (yhat, obs))
-
-#add time index to predictions
-fc_series = pd.Series(predictions,index=test.index)
-
-
-# evaluate forecasts
-rmse = sqrt(mean_squared_error(test, predictions))
-print('Test RMSE: %.3f' % rmse)
-# plot forecasts against actual outcomes
-pyplot.plot(train, color='blue')
-pyplot.plot(test, color='blue')
-pyplot.plot(fc_series, color='red')
-
-ax = pyplot.gca()
-ax.axes.xaxis.set_visible(False)
-
-#saving date
-save_series_to_csv(train, "train.csv")
-save_series_to_csv(test, "test.csv")
-save_series_to_csv(fc_series, "predictions.csv")
-
-#show graph
-pyplot.show()
+if __name__ == "__main__":
+    main()
